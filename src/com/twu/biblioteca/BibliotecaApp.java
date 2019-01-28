320980bb2b6c4937babbc0898fd5650f1c132fd2
Stivali Serna <stivali.s@thoughtworks.com>
2019-01-28 11:25:45 +0100

    Add welcome message

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -2,7 +2,7 @@ package com.twu.biblioteca;
 
 public class BibliotecaApp {
 
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
+    public static void main() {
+        System.out.println("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore");
     }
 }
--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -1,13 +1,25 @@
 package com.twu.biblioteca;
 
 
-import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.io.*;
 
 public class ExampleTest {
+    private static final String EOL = System.getProperty("line.separator");
 
     @Test
-    public void test() {
-        assertEquals(1, 1);
-    }
+    public void testWelcomeMessage() {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        PrintStream console = System.out;
+        try {
+           System.setOut(new PrintStream(bytes));
+           BibliotecaApp.main();
+        } finally {
+           System.setOut(console);
+        }
+        assertEquals(String.format(
+              "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore%n"),
+              bytes.toString());
+     }
 }
