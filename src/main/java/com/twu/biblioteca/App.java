407e4620c3ab5bff5f1d5d810163540b0d4c02ea
stivaliserna <stivaliserna08@gmail.com>
2019-01-28 12:54:16 +0100

    Setup gradle

--- src/main/java/com/twu/biblioteca/App.java
+++ src/main/java/com/twu/biblioteca/App.java
@@ -1,8 +1,7 @@
 package com.twu.biblioteca;
 
-public class BibliotecaApp {
-
-    public static void main() {
+public class App {
+    public static void main(String[] args) {
         System.out.println("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore");
     }
 }
